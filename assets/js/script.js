b0aaddb868da95ad04005871a562d459c3e90d93
DaSHdotHub <>
2023-10-26 04:52:55 +0000

    FEAT: Added algorithm to generate answer options, shuffle them and wired answers to buttons.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 //Read in the JSON file after DOM has loaded up.
 let data;
+let answerOptions = [];
+let displayOptions = [];
 
 document.addEventListener("DOMContentLoaded", () => {
     loadPhrases();
@@ -25,7 +27,44 @@ function onPhrasesLoaded() {
 
 function displayQuestion() {
     //Create random number between 0 and length of dataArray
-    let randomNumber = (Math.floor(Math.random() * data.length));
-    document.getElementById("display-question").textContent = data[randomNumber].altered_phrase;
-}
+    let randomQuestionNumber = (Math.floor(Math.random() * data.length));
+    let questionObject = data[randomQuestionNumber];
+    wrongOptions = data[randomQuestionNumber].wrong_options;
+    //Create possible answers array
+    generateAnswerObject();
+    console.log(displayOptions);
 
+    //Render data from JSON to DOM
+    document.getElementById("display-question").textContent = questionObject.altered_phrase;
+    document.getElementById("display-hint").textContent = questionObject.source;
+
+    function generateAnswerObject() {
+        //Pickup 3 random wrong answers from the dataset "wrong_options"
+        for (i = 0; i < 3; i++) {
+            randomNumber = Math.floor(Math.random() * wrongOptions.length);
+            answerOptions.push({
+                answerValue: wrongOptions[randomNumber],
+                answerCorrect: false
+            });
+            //remove picked up dataset for next iteration
+            wrongOptions.splice(randomNumber, 1);
+        }
+        //Add correct answer to set
+        answerOptions.push({
+            answerValue: questionObject.original_word,
+            answerCorrect: true
+        });
+        //Reorder elements
+        debugger;
+        arraySize = answerOptions.length;
+        for (i = 0; i < arraySize; i++) {
+            randomNumber = Math.floor(Math.random() * answerOptions.length);
+            displayOptions.push({
+                displayValue: answerOptions[randomNumber].answerValue,
+                displaCorrect: answerOptions[randomNumber].answerCorrect
+            });
+            //remove picked up dataset for next iteration
+            answerOptions.splice(randomNumber, 1);
+        }
+    }
+}
