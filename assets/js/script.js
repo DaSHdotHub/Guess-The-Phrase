dd1921e17265eee299e9e71bb2f431472bc92c6a
DaSHdotHub <>
2023-10-26 02:13:07 +0000

    FEAT: Added callback function, added displayQuestion feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,31 @@
-let submitElement = document.getElementById("submit").addEventListener("click", () => displaySum());
+//Read in the JSON file after DOM has loaded up.
+let data;
 
-function displaySum() {
-    let num1 = parseInt(document.getElementById("number1").value);
-    let num2 = parseInt(document.getElementById("number2").value);
-    document.getElementById("result").innerHTML = num1 + num2;
+document.addEventListener("DOMContentLoaded", () => {
+    loadPhrases();
+});
+
+async function loadPhrases() {
+    try {
+        //await fetch returns a promise in form of an http object which is rather a representation of the JSON, it does not contain it actually.
+        const response = await fetch('/assets/data/phrases.json');
+        //awair response returns a second promise, resolving the result of parsing the response body text as JSON.
+        data = await response.json();
+        //CallBack function that runs when data is loaded
+        onPhrasesLoaded();
+        //catch error when something went wrong fetching the JSON file
+    } catch (error) {
+        console.error("There was an error fetching the JSON file", error);
+    }
 }
+
+function onPhrasesLoaded() {
+    displayQuestion();
+}
+
+function displayQuestion() {
+    //Create random number between 0 and length of dataArray
+    let randomNumber = (Math.floor(Math.random() * data.length));
+    document.getElementById("display-question").textContent = data[randomNumber].altered_phrase;
+}
+
