734c177529030845ebce738f8243fa443e4e6e57
DaSHdotHub <>
2023-10-27 20:02:33 +0000

    REFACTOR: Adding changes from previous message

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,42 +11,11 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 
-async function loadPhrases() {
-    try {
-        //await fetch returns a promise in form of an http object which is rather a representation of the JSON, it does not contain it actually.
-        const response = await fetch('assets/data/phrases.json');
-        //awair response returns a second promise, resolving the result of parsing the response body text as JSON.
-        data = await response.json();
-        //CallBack function that runs when data is loaded
-        onPhrasesLoaded();
-        //catch error when something went wrong fetching the JSON file
-    } catch (error) {
-        console.error("There was an error fetching the JSON file", error);
-    }
-}
-
-
-//Algorithm to check if clcked button is 'correct' or 'incorrect'
-function checkAnswer(event) {
-    //Get clicked answer-button
-    if (event.target.classList.contains('answer-button')) {
-        const index = parseInt(event.target.id.toString().slice(-1)) - 1;
-        if (displayOptions[index].displayCorrect) {
-            alert("Genius! That was correct.");
-            incrementScore('correct');
-            displayNextQuestion();
-        } else {
-            alert("Try again");
-            incrementScore('incorrect');
-        }
-    }
-}
-
 function onPhrasesLoaded() {
     displayNextQuestion();
 }
 
-//Displays the question
+//Displays next question
 function displayNextQuestion() {
     //Create random number between 0 and length of dataArray
     randomQuestionNumber = (Math.floor(Math.random() * data.length));
@@ -64,37 +33,35 @@ function displayNextQuestion() {
     document.getElementById("answer3").textContent = displayOptions[2].displayValue;
     document.getElementById("answer4").textContent = displayOptions[3].displayValue;
 
-
-
     function generateAnswerObject() {
-        //Flush answerOptions and DisplayOptions before filling with new data
-        answerOptions = [];
-        displayOptions = [];
-        //Pickup 3 random wrong answers from the dataset "wrong_options"
-        for (i = 0; i < 3; i++) {
-            randomNumber = Math.floor(Math.random() * wrongOptions.length);
-            answerOptions.push({
-                answerValue: wrongOptions[randomNumber],
-                answerCorrect: false
-            });
-            //remove picked up dataset for next iteration
-            wrongOptions.splice(randomNumber, 1);
-        }
-        //Add correct answer to set
-        answerOptions.push({
-            answerValue: questionObject.original_word,
-            answerCorrect: true
-        });
-        //Reorder elements
-        arraySize = answerOptions.length;
-        for (i = 0; i < arraySize; i++) {
-            randomNumber = Math.floor(Math.random() * answerOptions.length);
-            displayOptions.push({
-                displayValue: answerOptions[randomNumber].answerValue,
-                displayCorrect: answerOptions[randomNumber].answerCorrect
-            });
-            //remove picked up dataset for next iteration
-            answerOptions.splice(randomNumber, 1);
+        // Grab 3 random wrong options
+        const shuffledWrongOptions = fisherYatesShuffle([...wrongOptions]);
+        const selectedWrongOptions = shuffledWrongOptions.slice(0, 3).map(option => ({
+            displayValue: option,
+            displayCorrect: false
+        }));
+
+        const correctOption = {
+            displayValue: data[randomQuestionNumber].original_word,
+            displayCorrect: true
+        };
+
+        displayOptions = fisherYatesShuffle([...selectedWrongOptions, correctOption]);
+    }
+
+}
+//Algorithm to check if clicked button is 'correct' or 'incorrect'
+function checkAnswer(event) {
+    //Get clicked answer-button
+    if (event.target.classList.contains('answer-button')) {
+        const index = parseInt(event.target.id.toString().slice(-1)) - 1;
+        if (displayOptions[index].displayCorrect) {
+            alert("Genius! That was correct.");
+            incrementScore('correct');
+            displayNextQuestion();
+        } else {
+            alert("Try again");
+            incrementScore('incorrect');
         }
     }
 }
@@ -103,4 +70,26 @@ function incrementScore(type) {
     const element = document.getElementById(type);
     const oldScore = parseInt(element.innerText);
     element.innerText = oldScore + 1;
+}
+//Shuffle an array with fisher yates algorithm
+function fisherYatesShuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+//Readin the JSON datafile
+async function loadPhrases() {
+    try {
+        //await fetch returns a promise in form of an http object which is rather a representation of the JSON, it does not contain it actually.
+        const response = await fetch('assets/data/phrases.json');
+        //awair response returns a second promise, resolving the result of parsing the response body text as JSON.
+        data = await response.json();
+        //CallBack function that runs when data is loaded
+        onPhrasesLoaded();
+        //catch error when something went wrong fetching the JSON file
+    } catch (error) {
+        console.error("There was an error fetching the JSON file", error);
+    }
 }
